6918cc65e64e0d370408b7b57b5fcecc5bc0d18e
Yaphet Ye <yeyuanfeng@vaimian.com>
2015-07-29 21:03:08 +0800

    modify weixin config

--- app/initializers/wxconfig.js
+++ app/initializers/wxconfig.js
@@ -6,8 +6,7 @@ wxconfig = {
    name: 'wxconfig',
    initialize: function() {
    		console.log("当前页面链接" + window.location.href);
-   		var url ="http://aym.vaimian.com/";
-    	Ember.$.getJSON('http://cts.vaimian.com/api/wechat/config?url=' + url).then(function(data) {    	   		
+    	Ember.$.getJSON('http://cts.vaimian.com/api/wechat/config?url=http://aym.vaimian.com/').then(function(data) {    	   		
 	    	wx.config(data);
 	    });
    }
