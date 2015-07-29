056031e8dab01f4d356bc007537db55563d75b31
Yaphet Ye <yeyuanfeng@vaimian.com>
2015-07-29 21:20:40 +0800

    不允许中奖

--- app/initializers/wxconfig.js
+++ app/initializers/wxconfig.js
@@ -8,7 +8,19 @@ wxconfig = {
    		console.log("当前页面链接" + window.location.href);
     	Ember.$.getJSON('http://cts.vaimian.com/api/wechat/config?url=http://aym.vaimian.com/').then(function(data) {    	   		
 	    	wx.config(data);
-	    });
+
+	    	wx.onMenuShareTimeline({
+			    title: "按1秒，赢胡巴", // 分享标题
+			    link: 'http://aym.vaimian.com', // 分享链接
+			    imgUrl: 'http://aym.vaimian.com/assets/images/logo.jpg', // 分享图标
+			    success: function () { 
+			        // 用户确认分享后执行的回调函数
+			    },
+			    cancel: function () { 
+			        // 用户取消分享后执行的回调函数
+			    }
+			});
+	    });    
    }
 };
 
