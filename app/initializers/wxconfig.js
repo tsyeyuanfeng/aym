3982a593ea7ad619d6faf7d054a375b44ce5de92
Yaphet Ye <yeyuanfeng@vaimian.com>
2015-07-29 20:55:40 +0800

    modify wxconfig

--- app/initializers/wxconfig.js
+++ app/initializers/wxconfig.js
@@ -6,8 +6,8 @@ wxconfig = {
    name: 'wxconfig',
    initialize: function() {
    		console.log("当前页面链接" + window.location.href);
-   		var url = encodeURI("http://www.vaimian.com:4200/");
-    	Ember.$.getJSON('http://cts.vaimian.com/api/wechat/config/' + url).then(function(data) {    	   		
+   		var url ="http://www.vaimian.com:4200/";
+    	Ember.$.getJSON('http://cts.vaimian.com/api/wechat/config?url=' + url).then(function(data) {    	   		
 	    	wx.config(data);
 	    });
    }
