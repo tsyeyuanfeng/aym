224d21036e2e1773028b9531f28059758cdb4f0d
Yaphet Ye <tsyeyuanfeng@126.com>
2016-05-07 22:47:13 +0800

    上传示例图片

--- README.md
+++ README.md
@@ -43,11 +43,5 @@ Make use of the many generators for code, try `ember help generate` for more det
 
 Specify what it takes to deploy your app.
 
-## Further Reading / Useful Links
-
-* [ember.js](http://emberjs.com/)
-* [ember-cli](http://www.ember-cli.com/)
-* Development Browser Extensions
-  * [ember inspector for chrome](https://chrome.google.com/webstore/detail/ember-inspector/bmdblncegkenkacieihfhpjfppoconhi)
-  * [ember inspector for firefox](https://addons.mozilla.org/en-US/firefox/addon/ember-inspector/)
-
+## Demo
+![demo](https://raw.githubusercontent.com/tsyeyuanfeng/aym/master/demo/demo.png)
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -28,39 +28,16 @@ indexController = Ember.Controller.extend({
 
 	actions: {
 		press: function() {
-			console.log("press ....");
 			var curTime = parseFloat((new Date().getTime() / 1000).toFixed(3));
 			this.set("startTime", curTime);
 		},
 
 		release: function() {
-			console.log("release....");
 			var startTime = this.get("startTime");
 			var curTime   = parseFloat((new Date().getTime() / 1000).toFixed(3));
 			var costTime  = parseFloat((curTime - startTime).toFixed(3));
-			
-			if(costTime !== 1.000) {
-				this.set("costTime", costTime);
-			}
-			else {
-				this.set("costTime", 0.999);
-			}
-
-			var title = "我按了" + this.get("costTime") + "秒，" + this.get("resultText");
-			
-			wx.onMenuShareTimeline({
-			    title: title, // 分享标题
-			    link: 'http://aym.vaimian.com', // 分享链接
-			    imgUrl: 'http://aym.vaimian.com/assets/images/logo.jpg', // 分享图标
-			    success: function () { 
-			        // 用户确认分享后执行的回调函数
-			    },
-			    cancel: function () { 
-			        // 用户取消分享后执行的回调函数
-			    }
-			});
+			this.set("costTime", costTime);
 		}
-
 	}
 
 });
